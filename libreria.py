2953636b5d406ebf0524b76b007c104fc191bf87
michaelgarcia1093 <michaelgarcia@utp.edu.co>
2016-11-04 01:08:40 -0500

    Añadido enemigo y su movimiento Añadido disparo a enemigos Añadido sprite de bala enemiga Añadido el sprite de enemigo

--- libreria.py
+++ libreria.py
@@ -281,6 +281,101 @@ class Jugador(pygame.sprite.Sprite):
                             if self.direccion == "abajo":
                                 self.rect.bottom=muro.rect.top
 
+class Enemigo(pygame.sprite.Sprite):
+    paredes=None
+    elementos=None
+    image_arriba = []
+    image_abajo =  []
+    image_derecha = []
+    image_izquierda=[]
+
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("archivos/imagenes/en.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+        self.direccion="arriba"
+        self.cont = 1 #Control de velocidad para los enemigos
+        self.vida = 100
+        self.flag = True
+        self.cont_disparos = 0 #Control de los disparos
+
+    def update_probabilidad(self):
+        self.probabilidad = random.randrange(0,100)
+
+    def get_direccion(self):
+        flagd=False
+        flagi=False
+        save = (self.rect.x,self.rect.y)
+
+        for muro in self.paredes:
+            self.rect.x+=20
+            if(checkCollision(self,muro)):
+                flagd = True
+                print "Con derecha"
+            self.rect.x-=40
+            if(checkCollision(self,muro)):
+                flagi = True
+                print "Con izquierda"
+        self.rect.x, self.rect.y = save[0],save[1]
+        if(not(flagd and flagi)):
+            self.direccion = "arriba"
+
+    def update(self):
+
+        if(self.cont == 0):
+            self.cont += 1
+            """self.update_probabilidad()
+            if(self.probabilidad > 0 and self.probabilidad < 50):
+                self.direccion = "derecha"
+            else:
+                self.direccion = "arriba"""
+
+            ls_choque=pygame.sprite.spritecollide(self, self.paredes, False)
+            for muro in ls_choque:
+                if(muro.tipo == "pared" or muro.tipo == "d_dinamita"):
+                    if self.direccion == "derecha":
+                       self.rect.right=muro.rect.left
+                       self.direccion = "izquierda"
+                    else:
+                        if self.direccion == "izquierda":
+                            self.rect.left=muro.rect.right
+                            self.direccion = "derecha"
+                        else:
+                            if self.direccion == "arriba":
+                                self.rect.bottom=muro.rect.top
+                                self.direccion = "abajo"
+                            else:
+                                if self.direccion == "abajo":
+                                    self.rect.top=muro.rect.bottom
+                                    self.direccion = "arriba"
+
+            if(self.direccion=="derecha"):
+                self.rect.x+=1
+            if(self.direccion=="izquierda"):
+                self.rect.x-=1
+            if(self.direccion == "arriba"):
+                self.rect.y+=1
+            if(self.direccion == "abajo"):
+                self.rect.y-=1
+        else:
+            if(self.cont >= 5):
+                self.cont = 0
+            else:
+                self.cont += 1
+
+        if(self.cont_disparos == 0):
+            self.cont_disparos+=1
+            b = Bullet("archivos/imagenes/bala_e.png", self.rect.x, self.rect.y, self.direccion)
+            ls_balas_e.add(b)
+            ls_todos.add(b)
+        else:
+            if(self.cont_disparos > 500):
+                self.cont_disparos=0
+            else:
+                self.cont_disparos+=1
+
 class Bullet(pygame.sprite.Sprite): #Hereda de la clase sprite
 
     def __init__(self, img_name, x,y, direccion): #img para cargar, y su padre(de donde debe salir la bala)
@@ -314,7 +409,7 @@ class Juego:
         print "historia"
 
     def nivel1(self):
-        global ANCHO,ALTO,pantalla,jugador,ls_todos,sub,tipo,ls_balas_boss,ls_muros,ls_elementos,ls_enemigos
+        global ANCHO,ALTO,pantalla,jugador,ls_todos,sub,tipo,ls_balas_boss,ls_muros,ls_elementos,ls_enemigos,ls_balas_e
         c_fondo = (255,0,0)
         ALTO = 600
         ANCHO = 800
@@ -336,6 +431,7 @@ class Juego:
         ls_elementos=pygame.sprite.Group()
         ls_jugador=pygame.sprite.Group()
         ls_bajasj = pygame.sprite.Group()
+        ls_balas_e = pygame.sprite.Group()
         terminar=False
         muerto = False
 
@@ -346,7 +442,18 @@ class Juego:
         ls_jugador.add(jugador)
         ls_todos.add(jugador)
 
+        pos_en = [(190, 53)]
+        dir_en = ["derecha"]
+        for i in range(len(pos_en)):
+            en = Enemigo(pos_en[i][0],pos_en[i][1])
+            en.paredes = ls_muros
+            en.direccion = dir_en[i]
+            ls_todos.add(en)
+            ls_enemigos.add(en)
+
         while not terminar:
+            if(pygame.mouse.get_pressed()):
+                print(pygame.mouse.get_pos())
             for event in pygame.event.get():
 
                 if event.type == pygame.QUIT:
@@ -390,10 +497,37 @@ class Juego:
                     if(checkCollision(b_j,muro)):
                         ls_bajasj.remove(b_j)
                         ls_todos.remove(b_j)
+                for enemigo in ls_enemigos:
+                    if(checkCollision(b_j,enemigo)):
+                        ls_bajasj.remove(b_j)
+                        ls_todos.remove(b_j)
+                        enemigo.vida -= random.randrange(10,20)
+                    if(enemigo.vida <= 0 ):
+                        ls_todos.remove(enemigo)
+                        ls_enemigos.remove(enemigo)
+                for b_e in ls_balas_e:
+                    if(checkCollision(b_j,b_e)):
+                        ls_bajasj.remove(b_j)
+                        ls_todos.remove(b_j)
+                        ls_balas_e.remove(b_e)
+                        ls_todos.remove(b_e)
+
+            for b_e in ls_balas_e:
+                for muro in ls_muros:
+                    if(checkCollision(b_e,muro)):
+                        ls_balas_e.remove(b_e)
+                        ls_todos.remove(b_e)
+                if(checkCollision(jugador,b_e)):
+                    jugador.vida -= random.randrange(10,20)
+
+
 
             """FIN ZONA DE COLLIDES"""
             ls_todos.update()
             ls_todos.draw(pantalla)
+            ls_elementos.draw(pantalla)
             ls_bajasj.draw(pantalla)
+            ls_balas_e.draw(pantalla)
+            ls_enemigos.draw(pantalla)
             ls_jugador.draw(pantalla)
             pygame.display.flip()
